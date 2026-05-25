267cac89995d6d50be66f83d29d06d052adcdc00
Adian <adianjay11@gmail.com>
2026-05-25 11:35:34 +0700

    add app.py streamlit and retrieval function

---
+++ src/retrieval.py
@@ -0,0 +1,27 @@
+from pinecone import Pinecone
+import os
+
+def init_pinecone():
+    pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+    index = pc.Index(os.getenv("INDEX_HOST"))
+    return pc, index
+
+def retrieve(pc, index, query, top_k=5):
+    embedding = pc.inference.embed(
+        model="llama-text-embed-v2",
+        inputs=[
+            query
+        ],
+        parameters={
+            "input_type": "query",
+            "dimension": 1024
+        }
+    )
+
+    results = index.query(
+        namespace="_default_",
+        vector=embedding[0]["values"],
+        top_k=top_k,
+        include_metadata=True
+    )
+    return results.get("matches", [])
